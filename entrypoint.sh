7f48e45ced2dd983541c7b6b07dd1b5a05a35722
Apocalypsor <37431235+Apocalypsor@users.noreply.github.com>
2021-07-30 20:16:48 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,5 +8,5 @@ ifinet=$(ifconfig $ifname | grep "inet addr:" | cut -d: -f2 | awk '{ print $1}')
 sed -i -e "s/__replace_me_ifname__/$ifname/" /etc/sockd.conf
 sed -i -e "s/__replace_me_ifinetip__/$ifinet/" /etc/tinyproxy.conf
 
-/usr/sbin/sockd
 /usr/bin/tinyproxy -c /etc/tinyproxy.conf
+/usr/sbin/sockd
